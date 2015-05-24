e89ec66d8c34d89a0cf10ec69f309f360e091ca0
a1237981379 <554286238@qq.com>
2015-05-24 10:13:43 +0800

    更新框架

--- app/app/actions/test.js
+++
@@ -1,8 +0,0 @@
-var server = rapid.use("rapid-httpserver");
-server.defineAction("test", function(){
-    //äÖÈ¾index.html
-    var content = this.render("/register");
-    //·¢ËÍµ½Ç°¶Ë
-    
-    this.send(content);
-});
